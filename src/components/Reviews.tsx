3bb0f9339fb15482409bb6aa85853767d395f6ad
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-18 23:49:57 +0600

    reviews components work on going

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -2,6 +2,16 @@ import React from "react";
 
 type Props = {};
 
+const PHONES = [
+    '/testimonials/1.jpg',
+    '/testimonials/2.jpg',
+    '/testimonials/3.jpg',
+    '/testimonials/4.jpg',
+    '/testimonials/5.jpg',
+    '/testimonials/6.jpg',
+]
+
+
 const Reviews = (props: Props) => {
   return <div>Reviews</div>;
 };
