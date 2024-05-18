646d9c95d1da99b18c8b77b221fed4456800aa09
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-18 23:37:03 +0600

    it is a case sell project

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+type Props = {
+    className?: string,
+    children: ReactNode
+};
+
+const MaxWidthWrapper = ({ className, children }: Props) => {
+    return (
+        <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
+            {children}
+        </div>
+    );
+};
+
+export default MaxWidthWrapper;
