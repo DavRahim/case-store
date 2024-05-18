ece56c46c695e300f31b104e8d99ccae4feadaef
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-19 00:10:30 +0600

    review components reviews column add

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from 'framer-motion'
 import { cn } from "@/lib/utils";
@@ -29,6 +29,52 @@ function splitArray<T>(array: Array<T>, numParts: number) {
 
     return result
 }
+
+function ReviewColumn({
+    reviews,
+    className,
+    reviewClassName,
+    msPerPixel = 0,
+}: {
+    reviews: string[]
+    className?: string
+    reviewClassName?: (reviewIndex: number) => string
+    msPerPixel?: number
+}) {
+    const columnRef = useRef<HTMLDivElement | null>(null)
+    const [columnHeight, setColumnHeight] = useState(0)
+    const duration = `${columnHeight * msPerPixel}ms`
+
+    useEffect(() => {
+        if (!columnRef.current) return
+
+        const resizeObserver = new window.ResizeObserver(() => {
+            setColumnHeight(columnRef.current?.offsetHeight ?? 0)
+        })
+
+        resizeObserver.observe(columnRef.current)
+
+        return () => {
+            resizeObserver.disconnect()
+        }
+    }, [])
+
+    return (
+        <div
+            ref={columnRef}
+            className={cn('animate-marquee space-y-8 py-4', className)}
+            style={{ '--marquee-duration': duration } as React.CSSProperties}>
+            {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
+                <Review
+                    key={reviewIndex}
+                    className={reviewClassName?.(reviewIndex % reviews.length)}
+                    imgSrc={imgSrc}
+                />
+            ))}
+        </div>
+    )
+}
+
 
 function ReviewGrid() {
     const containerRef = useRef<HTMLDivElement | null>(null)
