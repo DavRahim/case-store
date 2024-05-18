a2fc316a28ef9da4cd942caa6948151275c6ce1f
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-18 23:52:30 +0600

    review components some work done

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 type Props = {};
 
@@ -13,7 +16,15 @@ const PHONES = [
 
 
 const Reviews = (props: Props) => {
-  return <div>Reviews</div>;
+  return (
+      <MaxWidthWrapper className='relative max-w-5xl'>
+          <img
+              aria-hidden='true'
+              src='/what-people-are-buying.png'
+              className='absolute select-none hidden xl:block -left-32 top-1/3'
+          />
+      </MaxWidthWrapper>
+  );
 };
 
 export default Reviews;
