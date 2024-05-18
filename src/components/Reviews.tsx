e1f6a544e1422691428ac985ab346813ee9a6180
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-18 23:54:59 +0600

    review components grid add

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React from "react";
+import React, { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { cn } from "@/lib/utils";
 
 type Props = {};
 
@@ -13,6 +14,51 @@ const PHONES = [
     '/testimonials/5.jpg',
     '/testimonials/6.jpg',
 ]
+
+function ReviewGrid() {
+    const containerRef = useRef<HTMLDivElement | null>(null)
+    const isInView = useInView(containerRef, { once: true, amount: 0.4 })
+    const columns = splitArray(PHONES, 3)
+    const column1 = columns[0]
+    const column2 = columns[1]
+    const column3 = splitArray(columns[2], 2)
+
+    return (
+        <div
+            ref={containerRef}
+            className='relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3'>
+            {isInView ? (
+                <>
+                    <ReviewColumn
+                        reviews={[...column1, ...column3.flat(), ...column2]}
+                        reviewClassName={(reviewIndex) =>
+                            cn({
+                                'md:hidden': reviewIndex >= column1.length + column3[0].length,
+                                'lg:hidden': reviewIndex >= column1.length,
+                            })
+                        }
+                        msPerPixel={10}
+                    />
+                    <ReviewColumn
+                        reviews={[...column2, ...column3[1]]}
+                        className='hidden md:block'
+                        reviewClassName={(reviewIndex) =>
+                            reviewIndex >= column2.length ? 'lg:hidden' : ''
+                        }
+                        msPerPixel={15}
+                    />
+                    <ReviewColumn
+                        reviews={column3.flat()}
+                        className='hidden md:block'
+                        msPerPixel={10}
+                    />
+                </>
+            ) : null}
+            <div className='pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100' />
+            <div className='pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100' />
+        </div>
+    )
+}
 
 
 const Reviews = (props: Props) => {
