b658383d7055233f851e485f2b086115925b1ede
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-20 20:28:16 +0600

    design confi add

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,45 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import NextImage from 'next/image'
+
+interface DesignConfiguratorProps {
+    configId: string
+    imageUrl: string
+    imageDimensions: { width: number; height: number }
+}
+
+
+const DesignConfigurator = ({
+    configId,
+    imageUrl,
+    imageDimensions,
+}: DesignConfiguratorProps) => {
+    const { toast } = useToast()
+    const router = useRouter()
+
+    return (
+        <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+            <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+                <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+                    <AspectRatio
+                        // ref={phoneCaseRef}
+                        ratio={896 / 1831}
+                        className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
+                        <NextImage
+                            fill
+                            alt='phone image'
+                            src='/phone-template.png'
+                            className='pointer-events-none z-50 select-none'
+                        />
+                    </AspectRatio>
+                  
+                </div>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default DesignConfigurator;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,7 @@
+import { db } from "@/db";
 import { notFound } from "next/navigation";
 import React from "react";
+import DesignConfigurator from "./DesignConfigurator";
 
 interface PageProps {
     searchParams: {
@@ -9,11 +11,27 @@ interface PageProps {
 
 const Page = async ({ searchParams }: PageProps) => {
     const { id } = searchParams
-
     if (!id || typeof id !== 'string') {
         return notFound()
     }
-  return <div>Page</div>;
+
+    const configuration = await db.configuration.findUnique({
+        where: { id },
+    })
+
+    if (!configuration) {
+        return notFound()
+    }
+
+    const { imageUrl, width, height } = configuration
+
+    return (
+        <DesignConfigurator
+            configId={configuration.id}
+            imageDimensions={{ width, height }}
+            imageUrl={imageUrl}
+        />
+    );
 };
 
 export default Page;
