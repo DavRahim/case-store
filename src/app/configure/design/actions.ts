89d234f657018991f4b8fe1db9c3a3ed47d8cec6
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-21 20:02:36 +0600

    action add

--- src/app/configure/design/actions.ts
+++ src/app/configure/design/actions.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { db } from '@/db'
-import { CaseColor, CaseFinish, CaseMaterial;, PhoneModel } from '@prisma/client'
+import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from '@prisma/client'
 
 export type SaveConfigArgs = {
   color: CaseColor
