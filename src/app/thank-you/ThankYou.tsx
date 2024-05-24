8f1650f386008c47f62f46145548a2c3dad6488e
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-24 20:23:43 +0600

    thank page working on going

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -19,7 +19,9 @@ const ThankYou = (props: Props) => {
   return (
     <div className="bg-white">
       <div className='mx-auto max-w-3xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8'>
+        <div className='max-w-xl'>
 
+        </div>
       </div>
     </div>
   );
