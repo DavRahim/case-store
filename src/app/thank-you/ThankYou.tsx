16d7a47b815525347ed0fc13ee27fbc770b63a01
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-24 20:13:36 +0600

    thank page working on going

---
+++ src/app/thank-you/ThankYou.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from "react";
+
+type Props = {};
+
+const ThankYou = (props: Props) => {
+  const searchParams = useSearchParams()
+  return <div>ThankYou</div>;
+};
+
+export default ThankYou;
---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from "react";
+import ThankYou from "./ThankYou";
+
+type Props = {};
+
+const page = (props: Props) => {
+    return <Suspense>
+        <ThankYou />
+    </Suspense>;
+};
+
+export default page;
