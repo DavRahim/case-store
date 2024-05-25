f86de753d9ff3147557c7e17f6f3356066da27e8
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-25 21:19:18 +0600

    dashboard page working on going

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { db } from "@/db";
 import { formatPrice } from "@/lib/utils";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -102,6 +103,42 @@ const Page = async (props: Props) => {
                         </Card>
                     </div>
                     <h1 className='text-4xl font-bold tracking-tight'>Incoming orders</h1>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Customer</TableHead>
+                                <TableHead className='hidden sm:table-cell'>Status</TableHead>
+                                <TableHead className='hidden sm:table-cell'>
+                                    Purchase date
+                                </TableHead>
+                                <TableHead className='text-right'>Amount</TableHead>
+                            </TableRow>
+                        </TableHeader>
+
+                        <TableBody>
+                            {orders.map((order) => (
+                                <TableRow key={order.id} className='bg-accent'>
+                                    <TableCell>
+                                        <div className='font-medium'>
+                                            {order.shippingAddress?.name}
+                                        </div>
+                                        <div className='hidden text-sm text-muted-foreground md:inline'>
+                                            {order.user.email}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell className='hidden sm:table-cell'>
+                                        {/* <StatusDropdown id={order.id} orderStatus={order.status} /> */}
+                                    </TableCell>
+                                    <TableCell className='hidden md:table-cell'>
+                                        {order.createdAt.toLocaleDateString()}
+                                    </TableCell>
+                                    <TableCell className='text-right'>
+                                        {formatPrice(order.amount)}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </div>
             </div>
         </div>
