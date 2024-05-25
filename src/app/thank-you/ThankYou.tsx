a50ecd7430a4014c3c95d896c5b46743ebd282b3
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-25 19:51:18 +0600

    thank you page data destscting on client page

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -43,6 +43,9 @@ const ThankYou = (props: Props) => {
     )
   }
 
+  const { configuration, billingAddress, shippingAddress, amount } = data
+  const { color } = configuration
+
   return (
     <div className="bg-white">
       <div className='mx-auto max-w-3xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8'>
