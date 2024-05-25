d857ef2e7b5883b9100520072e4545474a6eb685
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-25 20:47:07 +0600

    admin dashboasard working start

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+
+return (
+<div className='flex min-h-screen w-full bg-muted/40'>
+
+</div>
+
+);
+};
+
+export default Page;
