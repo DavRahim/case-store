d9816560fa90fb2151b51364f482ca4a019a58b4
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-03 22:40:38 +0600

    some add

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -97,14 +97,10 @@ export async function POST(req: Request) {
 
 
         }
-
-
-
         return NextResponse.json({ result: event, ok: true })
 
     } catch (err) {
         console.error(err)
-
         return NextResponse.json(
             { message: 'Something went wrong', ok: false },
             { status: 500 }
